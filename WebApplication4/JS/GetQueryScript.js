028598fc69db80abf4b0df0069c134c68d5bd78b
Crosby-Titan <mazafaka2018huy@gmail.com>
2023-09-25 20:23:59 +0300

    Test

---
+++ WebApplication4/JS/GetQueryScript.js
@@ -0,0 +1,28 @@
+﻿const { get } = require("http");
+
+document.getElementById("sendQuery").addEventListener("click", Click);
+var searchElement = document.getElementById("queryString");
+var img = document.querySelector("img");
+
+async function Click() {
+    const request = await fetch("https://localhost:7135/",
+        {
+            method: "GET",
+            headers: { "Accept": "application/json", "Content-Type": "application/json" },
+            body: JSON.stringify({
+                QueryString: searchElement.value
+            })
+        }
+    );
+
+    while (true) {
+        const file = await request.blob();
+
+        if (file.size <= 0)
+            break;
+
+        img.src = URL.createObjectURL(file);
+
+    }
+    
+}
--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -2,14 +2,55 @@ namespace WebApplication4
 {
     public class Program
     {
+        private static IDictionary<string, string> _pictures { get; set; }  
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            _pictures = new Dictionary<string, string>
+            {
+                { "samurai", @"b79f9664257bf2836f7e7ca896f92244.jpg" }
+            };
+
+            app.MapGet("/", async (context) => 
+            {
+                context.Response.ContentType = "text/html; charset=utf-8;";
+
+                await context.Response.SendFileAsync(@"../HTML/index.html");
+            
+            });
+
+            app.MapGet("/search",async (context) =>
+            {
+                context.Request.ContentType = "application/json";
+
+                var query = await context.Request.ReadFromJsonAsync<QueryString>();
+
+                if (!_pictures.ContainsKey(query.Query))
+                {
+                    var a = new BinaryWriter(context.Response.Body);
+
+                    a.Write("Invalid request");
+
+                    a.Close();
+                }
+
+                using var binaryReader = new BinaryReader(
+                    File.Open(
+                        @$"C:\Users\CrosbyTitan\source\repos\WebApplication4\WebApplication4\Resources\
+                        {_pictures[query.Query ?? "null"]}",FileMode.Open));
+
+                await binaryReader.BaseStream.CopyToAsync(context.Response.Body);
+            });
 
             app.Run();
         }
+    }
+
+    [Serializable]
+    internal class QueryString
+    {
+        public string? Query { get; set; }
     }
 }
